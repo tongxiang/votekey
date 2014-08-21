5ff64e756b540619bffa576338520c4e468b432a
bpfarmer <bpfarmer@gmail.com>
2014-08-21 12:52:09 -0400

    adding user models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votekey::Application.config.secret_token = '15291dda215014f81d47c7292be9163875b8b5386119e9774d4afa73e0918831e52733072ef72094fb1fc00192d44bea0f8226cd86efa2565c354543d93eb0fc'
